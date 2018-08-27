3cce32451f6d99f2677c84367a7a7fb0347823e0
Mitch McClellan <mitchelljmcclellan@gmail.com>
2018-08-27 19:45:55 -0400

    Adding diffuse

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,19 @@ using namespace std;
 
 string filename = "hello.ppm";
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0f * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0f);
+    return p;
+}
+
 vec3 color(const ray &r, hitable *world) {
     hit_record rec;
-    if (world->hit(r, 0.0f, MAXFLOAT, rec)) {
-        return 0.5f * (rec.normal + vec3(1, 1, 1));
+    if (world->hit(r, 0.001f, MAXFLOAT, rec)) { // t_min set to near 0 to avoid shadow acne
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5f * color(ray(rec.p, target - rec.p), world);
     } else {
         // Background color: Lerp from blue to white
         vec3 unit_direction = unit_vector(r.direction());
@@ -57,9 +66,11 @@ int main() {
                 vec3 p = r.point_at_parameter(2.0f);
                 col += color(r, world);
             }
-
-            // Color scaled to 0 to 255
+            // Average color samples
             col /= float(ns);
+            // Gamma 2 correction, color^(1/2)
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+            // Color scaled to 0 to 255
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
